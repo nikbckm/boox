e4d961e3a312f4ac7903b5a106d79a12f7f80a9d
nikbckm <33151122+nikbckm@users.noreply.github.com>
2017-11-03 17:47:16 +0100

    Sort
    sort

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 	<meta charset="utf-8">
 	
+	
 	<style>
 		table, td, th {
 			padding: 10px;
@@ -28,26 +29,53 @@
 
 <body>
 	<h1>Boox Datenbank</h1>
-	<button type="button" class="btn btn-success">Nach Name ABC ordnen</button>
-	<button type="button" class="btn btn-success">Nach ID ordnen</button>
+	<a type="button" class="btn btn-success" href="index.php?abc_sort=true">Nach Name ABC ordnen</button>
+	<a type="button" class="btn btn-success" href="index.php?id_sort=true">Nach ID ordnen</button>
 	<table class="table table-striped">
 			<th>id</th>
 			<th>name</th>
 			<th>mail</th>
 			<th>password</th>
-				<?php
+			
+			<?php
+				if (isset($_GET['abc_sort'])) {
 					$stmt = "SELECT * FROM `user` ORDER BY name ASC";
-					$result = $link->query($stmt);
-
-					while ($row = mysqli_fetch_row($result)){
-						echo "<tr>\n";
-						echo "<td>" . $row[0] . "</td>\n";
-						echo "<td>" . $row[1] . "</td>\n";
-						echo "<td>" . $row[2] . "</td>\n";
-						echo "<td>" . $row[3] . "</td>\n";
-						echo "<tr>\n";
-					}
-				?>	
+						$result = $link->query($stmt);
+						while ($row = mysqli_fetch_row($result)){
+							echo "<tr>\n";
+							echo "<td>" . $row[0] . "</td>\n";
+							echo "<td>" . $row[1] . "</td>\n";
+							echo "<td>" . $row[2] . "</td>\n";
+							echo "<td>" . $row[3] . "</td>\n";
+							echo "<tr>\n";
+						}
+				} else if (isset($_GET['id_sort'])) {
+					$stmt = "SELECT * FROM `user` ORDER BY id ASC";
+						$result = $link->query($stmt);
+						while ($row = mysqli_fetch_row($result)){
+							echo "<tr>\n";
+							echo "<td>" . $row[0] . "</td>\n";
+							echo "<td>" . $row[1] . "</td>\n";
+							echo "<td>" . $row[2] . "</td>\n";
+							echo "<td>" . $row[3] . "</td>\n";
+							echo "<tr>\n";
+						}
+				} else {
+						$stmt = "SELECT * FROM `user`";
+						$result = $link->query($stmt);
+						while ($row = mysqli_fetch_row($result)){
+							echo "<tr>\n";
+							echo "<td>" . $row[0] . "</td>\n";
+							echo "<td>" . $row[1] . "</td>\n";
+							echo "<td>" . $row[2] . "</td>\n";
+							echo "<td>" . $row[3] . "</td>\n";
+							echo "<tr>\n";
+						}
+				}
+										
+			?>
+			
+					
 	</table>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
